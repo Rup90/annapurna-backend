bad372c3eaf90e993aa837d0f55a71a94f6d5a56
Rupayan Dey <rupayan.dey1990@gmail.com>
2020-07-09 18:12:48 +0530

    changes in db connection

--- src/db.ts
+++ src/db.ts
@@ -4,6 +4,6 @@ export default class Db {
     private mongoDb: string = 'mongodb+srv://annapurna:zdZfMCCmUgIDMKa7@cluster0-1jruh.mongodb.net/annapurna?retryWrites=true&w=majority';
 
     static async setupDb(db: Db): Promise<any> {
-        return await mongoose.connect(db.mongoDb, {useNewUrlParser: true});
+        return await mongoose.connect(db.mongoDb, {useNewUrlParser: true, useUnifiedTopology: true});
     }
 }
